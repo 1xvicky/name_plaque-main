2e16aab4d56a91ab56447583244ee881e66999b5
vicky <amosvictory82@gmail.com>
2022-10-15 14:19:35 +0100

    first commit

---
+++ name_plaque-main/buy_now.js
@@ -0,0 +1,23 @@
+// Project on arrow functions
+const buyNow = () =>{
+
+  const usrInput = document.querySelector('.field-input')
+  if (usrInput.value.length>15){
+    alert("too long")
+  }
+  else{
+    if (usrInput.value===""){
+      alert("Please enter name")
+    }
+    else if(usrInput.value==="Your Name"){
+      alert("Please enter your custom latters")
+  
+    }
+    else{
+      alert("Success")
+    }
+  }
+  
+  
+  }
+but.addEventListener("click", buyNow);
